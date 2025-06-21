34579620d2b409c49b2f1e7712e0ce9808a0c6fe
Thaddeus <thaddeuspearson@gmail.com>
2025-06-21 17:13:58 +0200

    add update_data to Easy_1 SQL exercises

---
+++ SQL_Fundamentals/Easy_1/update_data.sql
@@ -0,0 +1,7 @@
+/*
+It seems there was a mistake when we were inserting data in the birds table. 
+All the crows are actually ravens. Update the birds table so that the rows
+with a species of 'Crow' now read 'Raven'.
+*/
+
+UPDATE birds SET species TO 'Raven' WHERE species = 'Crow';
